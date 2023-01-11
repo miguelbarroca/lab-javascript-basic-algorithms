027b61931c252f50dce0b8b0e9748a48a3a5971c
Miguel Barroca <miguelbarroca@protonmail.com>
2023-01-11 15:57:04 +0000

    final iteration-2

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// Iteration 1: Names and Input
+// Iteration-1
+const hacker0 = "Maik";
+console.log(`The driver's name is ${hacker0}.`);
+const hacker1 = "Miguel";
+console.log(`The navigators' names are ${hacker1}.`);
 
-
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
+// Iteration-2
+if (hacker0.length > hacker1.length) {
+  console.log(`${hacker0} has the longest name, the length of ${hacker0} is ${hacker0.length}`)
+} else if ( hacker0.length < hacker1.length) {
+  console.log(`${hacker1} has the longest name, the length of ${hacker1} is ${hacker1.length}`)
+} else {
+  console.log(`the lengths are equal.`)
+}
